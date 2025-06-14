7f79eed8cd418cceb24a25f8bf5b0e42e7a225ed
adrianch99 <charrisadrianjose@gmail.com>
2025-06-14 17:43:45 -0500

    ----

--- server.js
+++ server.js
@@ -277,13 +277,20 @@ app.get('/api/pedidos-unitarios', async (req, res) => {
     }
 });
 
-app.get('/api/productos/:id', (req, res) => {
+app.get('/api/productos/:id', async (req, res) => {
     const productoId = req.params.id;
-    const producto = producto.find(p => p.id === parseInt(productoId));
-    if (producto) {
-        res.json(producto);
-    } else {
-        res.status(404).json({ error: 'Producto no encontrado' });
+
+    try {
+        const result = await pool.query('SELECT * FROM productos WHERE id = $1', [productoId]);
+
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]); // Devuelve el producto encontrado
+        } else {
+            res.status(404).json({ error: 'Producto no encontrado' }); // Manejo de error si no se encuentra el producto
+        }
+    } catch (error) {
+        console.error('Error al obtener el producto:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
     }
 });
 
